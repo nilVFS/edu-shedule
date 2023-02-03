954570eff1bc0f10f200898db750a5c3aa5a2e8c
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2023-02-03 06:00:43 +0300

    create templates for pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,26 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span><b>ПОИЯ-БЗ-19</b></span>,
+
+  search: {
+    placeholder: "Поиск..."
   },
-  chat: {
-    link: 'https://discord.com',
+
+  feedback: {
+    content: null
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Владислав Низьев 2023"
+  },
+
+  toc: {
+    title: "На этой странице"
+  },
+
+  editLink: {
+    text: null
   },
 }
 
