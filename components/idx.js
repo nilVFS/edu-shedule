73390840b0d8ab7d318c3af78c7f705665b7f466
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2023-03-13 17:24:46 +0300

    update

--- components/idx.js
+++
@@ -1,7 +0,0 @@
-const date1 = new Date('3/13/2023');
-const date2 = new Date();
-const diffTime = date2 - date1;
-const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-const ind = diffDays >= 0 ? diffDays : 0
-
-export default ind
